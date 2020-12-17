fc6183943bb3a26428188c8df5ae37203f38d3bd
Ally Kadel <akadel01@gmail.com>
2020-12-17 15:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let minVal = array[0];
+  let minIndex = 0;
+  for (let i=1; i<array.length; i++) {
+    if (array[i] < minVal) {
+      minVal = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(i-1,i)
+  return minVal;
 }
 
 function selectionSort(array){
-
+  let newArr = [];
+  while (array.length > 0) {
+    newArr.push(findMinAndRemove(array));
+    selectionSort(newArr);
+  }
+  return newArr;
 }
